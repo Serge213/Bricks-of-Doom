e237936d19d61df2c50b9e4d1de8ff4c8099b0cd
Bigtimbers <therealguitarman@gmail.com>
2016-08-10 17:20:04 -0700

    Added brick destruction and breaking sprites+code

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,29 +3,49 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+	public Sprite[] hitSprites;
+
 	private int timesHit;
-	private LevelManager;
+	private LevelManager levelManager;
 
 	// Use this for initialization
 	void Start () {
 		timesHit = 0;
-		LevelManager = GameObject.FindObjectOfType<LevelManager> ();
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			HandleHits ();
+		}
+
+	}
+
+	void HandleHits () {
 		timesHit++;
-		SimulateWin ();
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			Destroy (gameObject);
+		} else {
+			LoadSprites ();
+		}
+	}
+
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
 	}
 
 	// TODO Remove this method once we can actually win!
 	void SimulateWin() {
-		LevelManager.LoadNextLevel ();
+		levelManager.LoadNextLevel ();
 	}
 }
